9b1e38501b1cc207a28ceaf83bf39097b295c18d
Dmitrii Lifanov <lifanov.dv93@gmail.com>
2022-02-13 23:34:05 +0300

    Solved the second task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,15 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+    if (param === "asc") {
+        return sortByOrder(arr);
+    } else if (param === "desc") {
+        return sortByOrder(arr).reverse();
+    } else throw new Error(`Unknown type of sortion! Value: ${param} Type: ${typeof param}`)
+}
 
+function sortByOrder(arr) {
+    let sortedArr = arr.slice().sort((a, b) => a.localeCompare(b, 'ru', {caseFirst : "upper"}));
+    return sortedArr;
 }
