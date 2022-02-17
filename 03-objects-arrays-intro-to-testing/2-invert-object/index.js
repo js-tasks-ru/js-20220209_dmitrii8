808f481b55a551a8be7eae6591189b362ad13019
Dmitrii Lifanov <lifanov.dv93@gmail.com>
2022-02-17 23:22:25 +0300

    The second task of the trird block is solved

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,10 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if(typeof obj !== 'undefined') {
+        return Object.fromEntries(Object.entries(obj).map(arr => arr.reverse()));
+    } else {
+        return undefined;
+    }
+    
 }
