e7830040b7849a0ff9e60241e95a7739debc3df9
Dmitrii Lifanov <lifanov.dv93@gmail.com>
2022-02-19 13:24:54 +0300

    The third task of the third block is solved

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (typeof string === 'undefined') return undefined;
+    if (typeof size === 'undefined') return string;
+    if (string.length === 0 || size === 0) return "";
+    if (string.length <= size) return string;
 
+    const symbol = string[0];
+    let counter = 0;
+    let substr;
+    for (let letter of string) {
+        if (letter === symbol) ++counter;
+        else break;
+    }
+    if (counter >= size) {
+        substr = string.slice(0, size);
+    } else {
+        substr = string.slice(0, counter);
+    }
+    return substr + trimSymbols(string.slice(counter, string.length), size);
 }
